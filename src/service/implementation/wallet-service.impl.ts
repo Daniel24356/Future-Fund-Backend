b805b72431b0158be166282e48a2e164e24ae191
Uchenna99 <uchennaagbu@gmail.com>
2025-03-07 14:53:43 +0100

    deposit funds

--- src/service/implementation/wallet-service.impl.ts
+++ src/service/implementation/wallet-service.impl.ts
@@ -1,14 +1,40 @@
-import { Transaction } from "@prisma/client";
+import { DepositStatus, Transaction, TransactionType } from "@prisma/client";
 import { WalletService } from "../wallet.service";
+import { PaymentInitializationResponse, PaymentServiceImpl } from "../PaystackInitialization";
+import { db } from "../../configs/db";
+import { CustomError } from "../../exceptions/error/customError.error";
 
 
 export class WalletServiceImpl implements WalletService{
+    private paymentService = new PaymentServiceImpl();
+
     getUserBalance(userId: string): Promise<number> {
         throw new Error("Method not implemented.");
     }
-    depositFunds(userId: string, amount: number, description?: string): Promise<Transaction> {
-        throw new Error("Method not implemented.");
+
+    async depositFunds(userId: string, amount: number, description?: string): Promise<{transaction: Transaction, paymentResponse: PaymentInitializationResponse}> {
+        const findUser = await db.user.findUnique({
+            where: {id: userId}
+        })
+        if(!findUser) {
+            throw new CustomError(404, "User not found");
+        }
+        const paymentResponse = await this.paymentService.initializePayment(
+            findUser.email,
+            amount,
+            { transactionType: TransactionType.DEPOSIT }
+        )
+        const transaction = await db.transaction.create({
+            data: {
+                userId: findUser.id,
+                type: TransactionType.DEPOSIT,
+                status: DepositStatus.PENDING,
+                amount,
+            }
+        })
+        return {transaction, paymentResponse};
     }
+
     withdrawFunds(userId: string, amount: number, description?: string): Promise<Transaction> {
         throw new Error("Method not implemented.");
     }
--- src/service/wallet.service.ts
+++ src/service/wallet.service.ts
@@ -1,8 +1,9 @@
 import { Transaction, User } from "@prisma/client";
+import { PaymentInitializationResponse } from "./PaystackInitialization";
 
 export interface WalletService {
   getUserBalance(userId: string): Promise<number>;
-  depositFunds(userId: string, amount: number, description?: string): Promise<Transaction>;
+  depositFunds(userId: string, amount: number, description?: string): Promise<{transaction: Transaction, paymentResponse: PaymentInitializationResponse}>;
   withdrawFunds(userId: string, amount: number, description?: string): Promise<Transaction>;
   transferFunds(senderId: string, receiverEmail: string, amount: number): Promise<Transaction>;
   getUserTransactions(userId: string): Promise<Transaction[]>;
