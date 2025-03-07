4d49070111bf64ced291b2a266ffeba5f6ee01e0
Uchenna99 <uchennaagbu@gmail.com>
2025-03-07 20:55:12 +0100

    transfer funds completed

--- src/service/implementation/wallet-service.impl.ts
+++ src/service/implementation/wallet-service.impl.ts
@@ -3,6 +3,7 @@ import { WalletService } from "../wallet.service";
 import { PaymentInitializationResponse, PaymentServiceImpl } from "../PaystackInitialization";
 import { db } from "../../configs/db";
 import { CustomError } from "../../exceptions/error/customError.error";
+import { TransferFundsDTO } from "../../dto/wallet.dto";
 
 
 export class WalletServiceImpl implements WalletService{
@@ -38,9 +39,57 @@ export class WalletServiceImpl implements WalletService{
     withdrawFunds(userId: string, amount: number, description?: string): Promise<Transaction> {
         throw new Error("Method not implemented.");
     }
-    transferFunds(senderId: string, receiverEmail: string, amount: number): Promise<Transaction> {
-        throw new Error("Method not implemented.");
+
+    async transferFunds(data: TransferFundsDTO): Promise<Transaction|undefined> {
+        if(data.amount <= 0) {
+            throw new CustomError(400, "Amount must be greater than 0");
+        }
+        try {
+            const newTransaction = await db.$transaction(async()=>{
+                const findSender = await db.user.findUnique({
+                    where: {id: data.senderId}
+                })
+                if(!findSender) {
+                    throw new CustomError(404, "Sender not found");
+                }
+                if(findSender.balance < data.amount) {
+                    throw new CustomError(400, "Insufficient balance");
+                }
+
+                const findReceiver = await db.user.findUnique({
+                    where: {email: data.receiverEmail}
+                })
+                if(!findReceiver) {
+                    throw new CustomError(404, "Receiver not found");
+                }
+
+                await db.user.update({
+                    where: {id: data.senderId},
+                    data: {balance: findSender.balance - data.amount}
+                })
+
+                await db.user.update({
+                    where: {email: data.receiverEmail},
+                    data: {balance: findReceiver.balance + data.amount}
+                })
+
+                const transaction = await db.transaction.create({
+                    data: {
+                        userId: data.senderId,
+                        type: TransactionType.DEPOSIT,
+                        amount: data.amount,
+                        description: `Funds transfer from ${findSender.firstName +' '+ findSender.lastName} to ${findReceiver.firstName +' '+ findReceiver.lastName}`
+                    }
+                })
+                return transaction;
+            })
+            return newTransaction;
+
+        } catch (error) {
+            throw new CustomError(400, "Transaction failed");
+        }
     }
+
     getUserTransactions(userId: string): Promise<Transaction[]> {
         throw new Error("Method not implemented.");
     }
--- src/service/wallet.service.ts
+++ src/service/wallet.service.ts
@@ -1,10 +1,11 @@
 import { Transaction, User } from "@prisma/client";
 import { PaymentInitializationResponse } from "./PaystackInitialization";
+import { TransferFundsDTO } from "../dto/wallet.dto";
 
 export interface WalletService {
   getUserBalance(userId: string): Promise<number>;
   depositFunds(userId: string, amount: number, description?: string): Promise<{transaction: Transaction, paymentResponse: PaymentInitializationResponse}>;
   withdrawFunds(userId: string, amount: number, description?: string): Promise<Transaction>;
-  transferFunds(senderId: string, receiverEmail: string, amount: number): Promise<Transaction>;
+  transferFunds(data: TransferFundsDTO): Promise<Transaction|undefined>;
   getUserTransactions(userId: string): Promise<Transaction[]>;
 }
