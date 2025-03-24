e0395d2e9d64c2f433436764d92e313aae071918
Daniel24356 <igwedaniel128@gmail.com>
2025-03-24 03:48:38 -0700

    new commit

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,8 +1,8 @@
-import admin, { ServiceAccount } from "firebase-admin";
-import * as serviceAccount from "./future-fund-8c402-firebase-adminsdk-fbsvc-d3b09bf70d.json"; 
+// import admin, { ServiceAccount } from "firebase-admin";
+// import * as serviceAccount from "./future-fund-8c402-firebase-adminsdk-fbsvc-d3b09bf70d.json"; 
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as ServiceAccount),
+// });
 
-export default admin;
+// export default admin;
