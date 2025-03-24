3759eb951715ccfe1433f574db12d93f2c5b99ab
Daniel24356 <igwedaniel128@gmail.com>
2025-03-24 04:08:39 -0700

    Removed firebase config from tracking

--- src/configs/firebase.ts
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin";
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG || "{}");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-export default admin;
